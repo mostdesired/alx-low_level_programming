b0d393df9ffa00405fce74ced8038b6ab85edb27
Katlego <successfullkok@gmail.com>
2022-09-11 15:17:52 -0700

    c programm

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -this programs prints a string
+ *
+ * Description: using main this program prints
+ * the string "and that piece of art is useful"
+ *- Dora Korpar, 2015-10-19
+ * Return: 1
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 61);
+return (1);
+}
