5f37b2a5afa564150ae50bcc2778209bea2665b4
Katlego <successfullkok@gmail.com>
2022-09-11 12:14:49 -0700

    prints string using puts instead of printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - print string in put function
+*
+*Description:using the main function 
+*this program should print the string "Programming is like building a multilingual puzzle"
+*Return 0
+*/
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
