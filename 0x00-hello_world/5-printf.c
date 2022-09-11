f91e2130d1ea18f3537bf7dba172848e0eb107f4
Katlego <successfullkok@gmail.com>
2022-09-11 12:47:33 -0700

    prints string in print instead of puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print string in printf
+*
+* Description: using the main function this program should print the
+* string "with proper grammar, but the outcome is a piece of art,"
+* Return: 0
+*/
+int main(0)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
