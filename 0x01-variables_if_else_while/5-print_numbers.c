4cc03cb0ad306bf9924be36d867c27e62432ef2f
Katlego <successfullkok@gmail.com>
2022-09-12 12:17:36 -0700

    print numbers orm 0 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to 10
+ *
+ * Description: using main print numbers from 0 to 10
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%n", n);
+	}
+	printf("\n");
+	return (0);
+}
